135153c4f3152633a03a931f0fabc16521af526d
DenisYaroslavtsev <deniska7777@list.ru>
2024-06-28 17:04:19 +0300

    ДЗ

---
+++ module_2_5.py
@@ -0,0 +1,15 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+
+result_1 = get_matrix(2, 3, 10)
+result_2 = get_matrix(3, 4 ,5)
+result_3 = get_matrix(1, 9 ,8)
+print(result_1)
+print(result_2)
+print(result_3)
