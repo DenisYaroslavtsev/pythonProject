ba23ce9c282726bf1cc0a492667844c1c0f789d7
DenisYaroslavtsev <deniska7777@list.ru>
2024-06-28 17:04:50 +0300

    ДЗ

--- module_2_5.py
+++ module_2_5.py
@@ -9,7 +9,7 @@ def get_matrix(n, m, value):
 
 result_1 = get_matrix(2, 3, 10)
 result_2 = get_matrix(3, 4 ,5)
-result_3 = get_matrix(1, 9 ,8)
+result_3 = get_matrix(4, 9 ,8)
 print(result_1)
 print(result_2)
 print(result_3)
