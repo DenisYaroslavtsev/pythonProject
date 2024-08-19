f7a9a9bc82626504ba965935f32d4c73be702f51
DenisYaroslavtsev <deniska7777@list.ru>
2024-08-19 18:21:22 +0300

    Cложные моменты и исключения в стеке вызова ф-ий

--- module_8_2.py
+++ module_8_2.py
@@ -9,6 +9,7 @@ def personal_sum(numbers):
             print(f'Некорректный тип данных для подсчёта суммы - {i}')
     return result, incorrect_data
 
+
 def calculate_average(numbers):
     try:
         if not isinstance(numbers, (list, tuple)):
@@ -23,8 +24,8 @@ def calculate_average(numbers):
     except TypeError:
         return None
 
+
 print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
-
