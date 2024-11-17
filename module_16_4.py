898159fa995748ad550c9620117c8522519e2f6f
DenisYaroslavtsev <deniska7777@list.ru>
2024-11-17 19:27:53 +0300

    Модели данных Pydantic

---
+++ module_16_4.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, Path, HTTPException
+from pydantic import BaseModel
+from typing import Annotated, List
+
+app = FastAPI()
+users = []
+
+
+class User(BaseModel):
+    id: int = None
+    username: str
+    age: int
+
+
+@app.get("/users/")
+async def get_users() -> List[User]:
+    return users
+
+
+@app.post("/user/{username}/{age}")
+async def create_user(
+        username: Annotated[str, Path(min_length=5, max_length=20, description="Enter Username", example="UrbanUsers")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter Age", example="29")]) -> str:
+    new_id = 1 if not users else max(user.id for user in users) + 1
+    new_user = User(id=int(new_id), username=username, age=age)
+    users.append(new_user)
+    return f'User {new_user} is registered'
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_user(
+        user_id: Annotated[int, Path(ge=1, description="Enter User ID", example="2")],
+        username: Annotated[str, Path(min_length=5, max_length=20, description="Enter Username", example="UrbanUsers")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter Age", example="29")]) -> str:
+    try:
+        for user in users:
+            if user.id == user_id:
+                user.username = username
+                user.age = age
+                return f"The user {user_id} is updated."
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.delete("/user/{user_id}")
+async def delete_user(
+        user_id: Annotated[int, Path(ge=1, description="Enter User ID", example="2")]):
+    try:
+        for index, user in enumerate(users):
+            if user.id == user_id:
+                users.pop(index)
+        return f'User {user_id} has been delete'
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
