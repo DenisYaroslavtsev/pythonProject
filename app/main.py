a815b2ebd2c81bdf34869e6f3d0349d0fad3f7d0
DenisYaroslavtsev <deniska7777@list.ru>
2024-11-24 22:42:28 +0300

    Структура проекта. Маршруты и модели Pydantic

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routers import task, users
+
+app = FastAPI()
+
+
+@app.get('/')
+def welcome():
+    return {"message": "Welcome to Taskmanager"}
+
+
+app.include_router(task.router)
+app.include_router(users.router)
